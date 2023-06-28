9ef06dec5d3e94e2c55d6793f74cc9781dcc867f
Yunir <spire47911@yandex.ru>
2023-06-28 09:52:44 +0500

    Task45

---
+++ Task45/src/main/java/org/example/App.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("appcontext.xml");
+        Car car = context.getBean("car", Car.class);
+        System.out.println(car);
+    }
+}
